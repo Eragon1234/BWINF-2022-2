fd29c6dcede8149091072cff367e6092a343d2c1
Alexander Gerstberger <a.gerstberger@web.de>
2023-01-01 15:23:42 +0100

    accepting filename as program argument

--- Aufgabe3/Quelltext/main.go
+++ Aufgabe3/Quelltext/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"os"
+)
 
+var filename string
+
+func init() {
+	flag.StringVar(&filename, "f", "", "Name der Datei mit dem Pancake")
+	flag.StringVar(&filename, "file", "", "Name der Datei mit dem Pancake")
+	flag.StringVar(&filename, "filename", "", "Name der Datei mit dem Pancake")
+	flag.Parse()
+}
+
+func main() {
+	if filename == "" && len(os.Args) > 1 {
+		filename = os.Args[1]
+	} else {
+		flag.PrintDefaults()
+		return
+	}
 }
