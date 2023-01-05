4ef65263af3fa43d864c410132a013d1feb6e424
Alexander Gerstberger <a.gerstberger@web.de>
2023-01-05 19:19:42 +0100

    adding tests for the ParsePancakeFromReader function

--- Aufgabe3/Quelltext/pancake/pancake_test.go
+++ Aufgabe3/Quelltext/pancake/pancake_test.go
@@ -1,7 +1,9 @@
 package pancake
 
 import (
+	"io"
 	"reflect"
+	"strings"
 	"testing"
 )
 
@@ -86,6 +88,71 @@ func TestPancake_Push(t *testing.T) {
 
 			if !reflect.DeepEqual(p, tt.want) {
 				t.Errorf("ReverseSlice() = %v, want %v", p, tt.want)
+			}
+		})
+	}
+}
+
+func TestParsePancakeFromReader(t *testing.T) {
+	type args struct {
+		reader io.Reader
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    Pancake
+		wantErr bool
+	}{
+		{
+			name: "parse empty pancake",
+			args: args{
+				reader: strings.NewReader(""),
+			},
+			want:    Pancake{},
+			wantErr: false,
+		},
+		{
+			name: "parse pancake with 1 element",
+			args: args{
+				reader: strings.NewReader("1\n1"),
+			},
+			want:    Pancake{1},
+			wantErr: false,
+		},
+		{
+			name: "parse pancake with 2 elements",
+			args: args{
+				reader: strings.NewReader("2\n1\n2"),
+			},
+			want:    Pancake{2, 1},
+			wantErr: false,
+		},
+		{
+			name: "parse pancake with 3 elements",
+			args: args{
+				reader: strings.NewReader("3\n1\n2\n3"),
+			},
+			want:    Pancake{3, 2, 1},
+			wantErr: false,
+		},
+		{
+			name: "testing if invalid pancake returns an error",
+			args: args{
+				reader: strings.NewReader("this\nis\nan\ninvalid\npancake"),
+			},
+			want:    Pancake{},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ParsePancakeFromReader(tt.args.reader)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ParsePancakeFromReader() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ParsePancakeFromReader() got = %v, want %v", got, tt.want)
 			}
 		})
 	}
