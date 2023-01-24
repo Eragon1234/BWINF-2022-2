983070ad22db27d891c8c87d2810aadaa053bef8
Alexander Gerstberger <a.gerstberger@web.de>
2023-01-24 18:36:09 +0100

    making the brute force sort better by replacing the TicketSystem with a sync map and improving tracking of the current shortest solution with an extra atomic uint32

--- Aufgabe3/Quelltext/pancake/sort.go
+++ Aufgabe3/Quelltext/pancake/sort.go
@@ -4,6 +4,7 @@ import (
 	"Aufgabe3/utils"
 	"sort"
 	"sync"
+	"sync/atomic"
 )
 
 func FlipAfterBiggestSortAlgorithm(p Stack) SortSteps {
@@ -29,41 +30,52 @@ func FlipAfterBiggestSortAlgorithm(p Stack) SortSteps {
 }
 
 func BruteForceSort(p Stack) SortSteps {
-	var helper func(*sync.WaitGroup, *utils.TicketSystem[SortSteps], Stack, SortSteps)
-
-	helper = func(wg *sync.WaitGroup, ts *utils.TicketSystem[SortSteps], p Stack, steps SortSteps) {
+	var helper func(*sync.WaitGroup, *sync.Map, Stack, SortSteps, *atomic.Uint32)
+	helper = func(wg *sync.WaitGroup, syncMap *sync.Map, p Stack, steps SortSteps, shortest *atomic.Uint32) {
 		defer wg.Done()
 
 		// check current steps length is greater than or equal to the smallest steps in done
-		s := ts.GetDone()
-		sort.Slice(s, func(i, j int) bool { return len(s[i]) < len(s[j]) })
-		if len(s) > 0 && len(steps) >= len(s[0]) {
+		if uint32(len(steps)) >= shortest.Load() {
 			return
 		}
 
 		if sort.SliceIsSorted(p, func(i, j int) bool { return p[i] > p[j] }) {
-			ts.Put(steps)
+			l := uint32(len(steps))
+			syncMap.Store(l, steps)
 			return
 		}
 
-		for i := 0; i <= len(p); i++ {
+		if uint32(len(steps)) >= shortest.Load()+1 {
+			return
+		}
+
+		wg.Add(len(p))
+		// running the for loop in reverse because I think that flipping more pancakes has a higher chance of sorting the stack
+		// the loop doesn't include 0 because it has the same as flipping 1
+		for i := len(p); i > 0; i-- {
 			pancake := p.Copy()
 			pancake.Flip(i)
 
-			wg.Add(1)
-			go helper(wg, ts, pancake, append(steps, i))
+			go helper(wg, syncMap, pancake, append(steps, i), shortest)
 		}
 	}
 
 	var wg sync.WaitGroup
-	var ts utils.TicketSystem[SortSteps]
+	var syncMap sync.Map
+
+	var shortest atomic.Uint32
+	// setting the shortest number of steps by default to the length of the stack - 1
+	// because only one element would be left and the whole stack would be sorted
+	shortest.Store(uint32(len(p) - 1))
 
 	wg.Add(1)
-	go helper(&wg, &ts, p, make([]int, 0))
+	go helper(&wg, &syncMap, p, make([]int, 0), &shortest)
 
 	wg.Wait()
 
-	steps := ts.GetDone()
-	sort.Slice(steps, func(i, j int) bool { return len(steps[i]) < len(steps[j]) })
-	return steps[0]
+	value, ok := syncMap.Load(shortest.Load())
+	if ok {
+		return value.(SortSteps)
+	}
+	return nil
 }
