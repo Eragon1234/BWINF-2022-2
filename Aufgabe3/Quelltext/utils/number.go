fbaf420901255759aed16d6e4fea40b5eb7eaba9
Alexander Gerstberger <a.gerstberger@web.de>
2023-01-25 18:46:41 +0100

    adding UnsignedNumber, SignedNumber and FloatNumber

--- Aufgabe3/Quelltext/utils/number.go
+++ Aufgabe3/Quelltext/utils/number.go
@@ -1,5 +1,17 @@
 package utils
 
 type Number interface {
-	int | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64 | uintptr | float32 | float64
+	UnsignedNumber | SignedNumber | FloatNumber
+}
+
+type UnsignedNumber interface {
+	uint | uint8 | uint16 | uint32 | uint64 | uintptr
+}
+
+type SignedNumber interface {
+	int | int8 | int16 | int32 | int64
+}
+
+type FloatNumber interface {
+	float32 | float64
 }
