39164bce2c4fcb824e74e94cbf764cb1c21a52f0
Alexander Gerstberger <a.gerstberger@web.de>
2023-01-26 21:34:59 +0100

    fixing a mistake in the tests

--- Aufgabe3/Quelltext/pancake/pancake_test.go
+++ Aufgabe3/Quelltext/pancake/pancake_test.go
@@ -146,7 +146,7 @@ func TestParse(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := Parse(tt.args.reader)
+			got, err := Parse[int](tt.args.reader)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("Parse() error = %v, wantErr %v", err, tt.wantErr)
 				return
