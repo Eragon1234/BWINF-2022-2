8c9951dca57d691400bd8c36c4978b152ddd0cdd
Alexander Gerstberger <a.gerstberger@web.de>
2023-02-03 08:20:28 +0100

    adding methods to weighted graph data structure

--- Aufgabe1/Quelltext/graph/graph.go
+++ Aufgabe1/Quelltext/graph/graph.go
@@ -7,6 +7,7 @@ type WeightedGraph[M any, T any] struct {
 
 func NewWeightedGraph[M any, T any](size int) *WeightedGraph[M, T] {
 	graph := &WeightedGraph[M, T]{
+		Vertices:        make([]Vertex[M], 0, size),
 		adjacencyMatrix: make([][]Edge[T], size),
 	}
 
@@ -15,4 +16,21 @@ func NewWeightedGraph[M any, T any](size int) *WeightedGraph[M, T] {
 	}
 
 	return graph
+}
+
+func (g *WeightedGraph[M, T]) AddVertex(value M) {
+	g.Vertices = append(g.Vertices, Vertex[M]{Index: len(g.Vertices), Value: value})
+	g.adjacencyMatrix = append(g.adjacencyMatrix, make([]Edge[T], len(g.Vertices)))
+	for i := range g.adjacencyMatrix {
+		g.adjacencyMatrix[i] = append(g.adjacencyMatrix[i], Edge[T]{})
+	}
+}
+
+func (g *WeightedGraph[M, T]) AddEdge(from, to int, weight T) {
+	g.adjacencyMatrix[from][to] = Edge[T]{From: from, To: to, Weight: weight}
+	g.adjacencyMatrix[to][from] = Edge[T]{From: to, To: from, Weight: weight}
+}
+
+func (g *WeightedGraph[M, T]) GetEdge(from, to int) Edge[T] {
+	return g.adjacencyMatrix[from][to]
 }
