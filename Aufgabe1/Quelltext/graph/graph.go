ecba45a7e9e3ffad98571b49aacad47757c44e70
Alexander Gerstberger <a.gerstberger@web.de>
2023-02-09 17:49:28 +0100

    refactorings of graph type names

--- Aufgabe1/Quelltext/graph/graph.go
+++ Aufgabe1/Quelltext/graph/graph.go
@@ -1,36 +1,36 @@
 package graph
 
-type WeightedGraph[M any, T any] struct {
-	Vertices        map[string]Vertex[M]
-	adjacencyMatrix [][]Edge[T]
+type WeightedGraph[Data any, Weight any] struct {
+	Vertices        map[string]Vertex[Data]
+	adjacencyMatrix [][]Edge[Weight]
 }
 
-func NewWeightedGraph[M any, T any](size int) *WeightedGraph[M, T] {
-	graph := &WeightedGraph[M, T]{
-		Vertices:        make(map[string]Vertex[M], size),
-		adjacencyMatrix: make([][]Edge[T], size),
+func NewWeightedGraph[Data any, Weight any](size int) *WeightedGraph[Data, Weight] {
+	graph := &WeightedGraph[Data, Weight]{
+		Vertices:        make(map[string]Vertex[Data], size),
+		adjacencyMatrix: make([][]Edge[Weight], size),
 	}
 
 	for i := range graph.adjacencyMatrix {
-		graph.adjacencyMatrix[i] = make([]Edge[T], size)
+		graph.adjacencyMatrix[i] = make([]Edge[Weight], size)
 	}
 
 	return graph
 }
 
-func (g *WeightedGraph[M, T]) AddVertex(name string, value M) {
-	g.Vertices[name] = Vertex[M]{Name: name, Value: value}
-	g.adjacencyMatrix = append(g.adjacencyMatrix, make([]Edge[T], len(g.Vertices)))
+func (g *WeightedGraph[D, W]) AddVertex(name string, value D) {
+	g.Vertices[name] = Vertex[D]{Name: name, Value: value}
+	g.adjacencyMatrix = append(g.adjacencyMatrix, make([]Edge[W], len(g.Vertices)))
 	for i := range g.adjacencyMatrix {
-		g.adjacencyMatrix[i] = append(g.adjacencyMatrix[i], Edge[T]{})
+		g.adjacencyMatrix[i] = append(g.adjacencyMatrix[i], Edge[W]{})
 	}
 }
 
-func (g *WeightedGraph[M, T]) AddEdge(from, to int, weight T) {
-	g.adjacencyMatrix[from][to] = Edge[T]{From: from, To: to, Exists: true, Weight: weight}
-	g.adjacencyMatrix[to][from] = Edge[T]{From: to, To: from, Exists: true, Weight: weight}
+func (g *WeightedGraph[D, W]) AddEdge(from, to int, weight W) {
+	g.adjacencyMatrix[from][to] = Edge[W]{From: from, To: to, Exists: true, Weight: weight}
+	g.adjacencyMatrix[to][from] = Edge[W]{From: to, To: from, Exists: true, Weight: weight}
 }
 
-func (g *WeightedGraph[M, T]) GetEdge(from, to int) Edge[T] {
+func (g *WeightedGraph[D, W]) GetEdge(from, to int) Edge[W] {
 	return g.adjacencyMatrix[from][to]
 }
