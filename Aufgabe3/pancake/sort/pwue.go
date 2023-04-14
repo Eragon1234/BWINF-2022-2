c37558c07f2d837fd60c922a83be39775d7456cb
Alexander Gerstberger <a.gerstberger@web.de>
2023-04-14 22:08:50 +0200

    moving permutation into its own file

--- Aufgabe3/pancake/sort/pwue.go
+++ Aufgabe3/pancake/sort/pwue.go
@@ -2,6 +2,7 @@ package sort
 
 import (
 	"BWINF/Aufgabe3/pancake"
+	"BWINF/utils"
 	"BWINF/utils/slice"
 )
 
@@ -12,7 +13,7 @@ func CalculatePWUE(n int) (stack pancake.Stack[int8], sortSteps pancake.SortStep
 	nums := slice.MakeFunc(n, func(i int) int {
 		return i + 1
 	})
-	perm := NewPermutation(nums)
+	perm := utils.NewPermutation(nums)
 	for p := nums; p != nil; p = perm.Next() {
 		s := slice.Map(p, func(i int) int8 {
 			return int8(i)
@@ -24,45 +25,4 @@ func CalculatePWUE(n int) (stack pancake.Stack[int8], sortSteps pancake.SortStep
 		}
 	}
 	return stack, sortSteps
-}
-
-type Permutation struct {
-	original    []int
-	permutation []int
-}
-
-func NewPermutation(s []int) Permutation {
-	return Permutation{
-		original:    s,
-		permutation: make([]int, len(s)),
-	}
-}
-
-// Next returns the next permutation or nil if there are no more permutations.
-// The permutations don't include the original.
-func (p *Permutation) Next() []int {
-	nextPerm(p.permutation)
-	ok := p.permutation[0] < len(p.permutation)
-	if !ok {
-		return nil
-	}
-	return getPerm(p.original, p.permutation)
-}
-
-func nextPerm(p []int) {
-	for i := len(p) - 1; i >= 0; i-- {
-		if i == 0 || p[i] < len(p)-i-1 {
-			p[i]++
-			return
-		}
-		p[i] = 0
-	}
-}
-
-func getPerm(orig, p []int) []int {
-	result := append([]int{}, orig...)
-	for i, v := range p {
-		result[i], result[i+v] = result[i+v], result[i]
-	}
-	return result
 }
---
+++ utils/permutation.go
@@ -0,0 +1,42 @@
+package utils
+
+type Permutation struct {
+	original    []int
+	permutation []int
+}
+
+func NewPermutation(s []int) Permutation {
+	return Permutation{
+		original:    s,
+		permutation: make([]int, len(s)),
+	}
+}
+
+// Next returns the next permutation or nil if there are no more permutations.
+// The permutations don't include the original.
+func (p *Permutation) Next() []int {
+	nextPerm(p.permutation)
+	ok := p.permutation[0] < len(p.permutation)
+	if !ok {
+		return nil
+	}
+	return getPerm(p.original, p.permutation)
+}
+
+func nextPerm(p []int) {
+	for i := len(p) - 1; i >= 0; i-- {
+		if i == 0 || p[i] < len(p)-i-1 {
+			p[i]++
+			return
+		}
+		p[i] = 0
+	}
+}
+
+func getPerm(orig, p []int) []int {
+	result := append([]int{}, orig...)
+	for i, v := range p {
+		result[i], result[i+v] = result[i+v], result[i]
+	}
+	return result
+}
