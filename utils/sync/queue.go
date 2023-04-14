e2e77fe416c7ea00f39d924310fa05c8cd465581
Alexander Gerstberger <a.gerstberger@web.de>
2023-04-14 22:00:19 +0200

    changing the implementation of queue back to a mutex from channel-based locking

--- utils/sync/queue.go
+++ utils/sync/queue.go
@@ -1,48 +1,31 @@
 package sync
 
 import (
-	"BWINF/utils"
 	"BWINF/utils/queue"
+	"sync"
 )
 
-type Representer interface {
-	Represent() string
+// PriorityQueue is a wrapper around queue.PriorityQueue that allows safe use by multiple goroutines without additional locking or coordination.
+type PriorityQueue[T any] struct {
+	mu sync.Mutex
+	pg queue.PriorityQueue[T]
 }
 
-type PriorityQueue[T Representer] struct {
-	lock    chan struct{}
-	pg      queue.PriorityQueue[T]
-	visited utils.Set[string]
-}
-
-func NewPriorityQueue[T Representer]() *PriorityQueue[T] {
-	lock := make(chan struct{}, 1)
-	lock <- struct{}{}
+func NewPriorityQueue[T any]() *PriorityQueue[T] {
 	return &PriorityQueue[T]{
-		lock:    lock,
-		pg:      queue.PriorityQueue[T]{},
-		visited: utils.Set[string]{},
+		pg: queue.PriorityQueue[T]{},
 	}
 }
 
 func (pq *PriorityQueue[T]) Push(val queue.Item[T]) {
-	<-pq.lock
-	defer func() {
-		pq.lock <- struct{}{}
-	}()
-	representation := val.Value.Represent()
-	if pq.visited.Contains(representation) {
-		return
-	}
-	pq.visited.Add(representation)
+	pq.mu.Lock()
+	defer pq.mu.Unlock()
 	pq.pg.Push(val)
 }
 
 func (pq *PriorityQueue[T]) Pop() (queue.Item[T], bool) {
-	<-pq.lock
-	defer func() {
-		pq.lock <- struct{}{}
-	}()
+	pq.mu.Lock()
+	defer pq.mu.Unlock()
 	return pq.pg.Pop()
 }
 
