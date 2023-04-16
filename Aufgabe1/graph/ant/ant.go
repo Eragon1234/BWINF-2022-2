2f68fd81109edd8b5c71d72630b417853b442875
Alexander Gerstberger <a.gerstberger@web.de>
2023-04-16 21:24:31 +0200

    moving VisitAllAntColonyOptimization to its own file

--- Aufgabe1/graph/ant/ant.go
+++ Aufgabe1/graph/ant/ant.go
@@ -8,81 +8,10 @@ import (
 	"BWINF/pkg/slice"
 	"fmt"
 	"math/rand"
-	"os"
-	"sort"
 	"sync"
 )
 
-func VisitAllAntColonyOptimization(cfg Config, g graph.WeightedGraph[vector.Coordinate, graph.DistanceAngle]) []graph.Edge[PheromoneDistanceAngle, vector.Coordinate] {
-	pheromoneGraph := transformGraph(g)
-
-	var totalEdgeLength float64
-	for _, v := range g.Vertices {
-		for _, e := range g.GetEdges(v) {
-			totalEdgeLength += e.Weight.Distance
-		}
-	}
-
-	// we add a start vertex to the graph that has length 0 to all other vertices
-	// this is necessary because the ant colony optimization algorithm needs a start vertex
-	pheromoneGraph.AddVertex("start", vector.Coordinate{})
-	for _, v := range pheromoneGraph.Vertices {
-		pheromoneGraph.AddEdge(pheromoneGraph.Vertices["start"], v, PheromoneDistanceAngle{
-			Pheromone:     0,
-			DistanceAngle: graph.DistanceAngle{Distance: 0, Angle: 0},
-		})
-	}
-	shortestPath := transformEdges(graph.VisitAllShortestEdge(g))
-	updatePheromone(cfg, totalEdgeLength, pheromoneGraph, shortestPath)
-
-	shortestPath = pheromoneGraph.GetEdges(pheromoneGraph.Vertices["210.000000 30.000000"])
-
-	ants := slice.MakeFunc(cfg.NumOfAnts, func(i int) Ant {
-		return *NewAnt(i, cfg)
-	})
-
-	var wg sync.WaitGroup
-	for i := 0; i < cfg.NumOfIterations; i++ {
-		fmt.Fprintln(os.Stderr, "iteration, bestLength:", i, LengthOfPheromonePath(shortestPath))
-
-		result := make(chan AntResult, cfg.NumOfAnts)
-		wg.Add(len(ants))
-		for _, ant := range ants {
-			go ant.Run(&wg, pheromoneGraph, result, LengthOfPheromonePath(shortestPath))
-		}
-		wg.Wait()
-		close(result)
-
-		var results []AntResult
-		for path := range result {
-			results = append(results, path)
-		}
-
-		sort.Slice(results, func(i, j int) bool {
-			return LengthOfPheromonePath(results[i].Path) < LengthOfPheromonePath(results[j].Path)
-		})
-
-		var eliteAnts []Ant
-
-		eliteIndex := int(float64(len(results)) * cfg.EliteProportion)
-		for _, antResult := range results[:eliteIndex] {
-			eliteAnts = append(eliteAnts, ants[antResult.Index])
-			updatePheromone(cfg, totalEdgeLength, pheromoneGraph, antResult.Path)
-			if LengthOfPheromonePath(antResult.Path) < LengthOfPheromonePath(shortestPath) {
-				shortestPath = antResult.Path
-			}
-		}
-		for _, loserAnt := range ants[eliteIndex:] {
-			randomEliteAnt := eliteAnts[rand.Intn(len(eliteAnts))]
-			loserAnt.Optimize(randomEliteAnt)
-		}
-
-		fmt.Fprint(os.Stderr, "\u001B[1A\u001B[K")
-	}
-	return shortestPath
-}
-
-type AntResult struct {
+type antResult struct {
 	Path  []graph.Edge[PheromoneDistanceAngle, vector.Coordinate]
 	Index int
 }
@@ -96,7 +25,7 @@ func NewAnt(i int, cfg Config) *Ant {
 	return &Ant{Index: i, cfg: cfg}
 }
 
-func (a *Ant) Run(wg *sync.WaitGroup, g *graph.WeightedGraph[vector.Coordinate, PheromoneDistanceAngle], result chan<- AntResult, shortestLength float64) {
+func (a *Ant) Run(wg *sync.WaitGroup, g *graph.WeightedGraph[vector.Coordinate, PheromoneDistanceAngle], result chan<- antResult, shortestLength float64) {
 	defer wg.Done()
 
 	var visited set.Set[*graph.Vertex[vector.Coordinate]]
@@ -134,7 +63,7 @@ func (a *Ant) Run(wg *sync.WaitGroup, g *graph.WeightedGraph[vector.Coordinate, 
 		path = append(path, edge)
 	}
 	if visited.Size() == len(g.Vertices) {
-		result <- AntResult{
+		result <- antResult{
 			Path:  path,
 			Index: a.Index,
 		}
--- Aufgabe1/graph/ant/visit.go
+++ Aufgabe1/graph/ant/visit.go
@@ -1,1 +1,81 @@
 package ant
+
+import (
+	"BWINF/Aufgabe1/graph"
+	"BWINF/Aufgabe1/vector"
+	"BWINF/pkg/slice"
+	"fmt"
+	"math/rand"
+	"os"
+	"sort"
+	"sync"
+)
+
+func VisitAllAntColonyOptimization(cfg Config, g graph.WeightedGraph[vector.Coordinate, graph.DistanceAngle]) []graph.Edge[PheromoneDistanceAngle, vector.Coordinate] {
+	pheromoneGraph := transformGraph(g)
+
+	var totalEdgeLength float64
+	for _, v := range g.Vertices {
+		for _, e := range g.GetEdges(v) {
+			totalEdgeLength += e.Weight.Distance
+		}
+	}
+
+	// we add a start vertex to the graph that has length 0 to all other vertices
+	// this is necessary because the ant colony optimization algorithm needs a start vertex
+	pheromoneGraph.AddVertex("start", vector.Coordinate{})
+	for _, v := range pheromoneGraph.Vertices {
+		pheromoneGraph.AddEdge(pheromoneGraph.Vertices["start"], v, PheromoneDistanceAngle{
+			Pheromone:     0,
+			DistanceAngle: graph.DistanceAngle{Distance: 0, Angle: 0},
+		})
+	}
+	shortestPath := transformEdges(graph.VisitAllShortestEdge(g))
+	updatePheromone(cfg, totalEdgeLength, pheromoneGraph, shortestPath)
+
+	shortestPath = pheromoneGraph.GetEdges(pheromoneGraph.Vertices["210.000000 30.000000"])
+
+	ants := slice.MakeFunc(cfg.NumOfAnts, func(i int) Ant {
+		return *NewAnt(i, cfg)
+	})
+
+	var wg sync.WaitGroup
+	for i := 0; i < cfg.NumOfIterations; i++ {
+		fmt.Fprintln(os.Stderr, "iteration, bestLength:", i, LengthOfPheromonePath(shortestPath))
+
+		result := make(chan antResult, cfg.NumOfAnts)
+		wg.Add(len(ants))
+		for _, ant := range ants {
+			go ant.Run(&wg, pheromoneGraph, result, LengthOfPheromonePath(shortestPath))
+		}
+		wg.Wait()
+		close(result)
+
+		var results []antResult
+		for path := range result {
+			results = append(results, path)
+		}
+
+		sort.Slice(results, func(i, j int) bool {
+			return LengthOfPheromonePath(results[i].Path) < LengthOfPheromonePath(results[j].Path)
+		})
+
+		var eliteAnts []Ant
+
+		eliteIndex := int(float64(len(results)) * cfg.EliteProportion)
+		for _, antResult := range results[:eliteIndex] {
+			eliteAnts = append(eliteAnts, ants[antResult.Index])
+			updatePheromone(cfg, totalEdgeLength, pheromoneGraph, antResult.Path)
+			if LengthOfPheromonePath(antResult.Path) < LengthOfPheromonePath(shortestPath) {
+				shortestPath = antResult.Path
+			}
+		}
+		for _, loserAnt := range ants[eliteIndex:] {
+			randomEliteAnt := eliteAnts[rand.Intn(len(eliteAnts))]
+			loserAnt.Optimize(randomEliteAnt)
+		}
+
+		fmt.Fprint(os.Stderr, "\u001B[1A\u001B[K")
+	}
+	return shortestPath
+}
